b03cc0c4f92fb3912ac8d9f1373cfef2782306d6
joshuabenedict-665 <joshuabenedict665@gmail.com>
2025-07-28 18:46:55 +0530

    added dummy model for test

--- backend/ml/predictor.py
+++ backend/ml/predictor.py
@@ -4,3 +4,8 @@ def match_surplus_to_needy(food_type, quantity, location):
         "distance_km": 2.4,
         "urgency_level": "High"
     }
+def classify_food(data: dict) -> str:
+    # Dummy logic (can be replaced with actual model)
+    if data["hours_since_prepared"] < 4 and data["temperature"] < 35:
+        return "Safe"
+    return "Unsafe"
