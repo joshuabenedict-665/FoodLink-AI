96a1c84132151b24618bbf448ec88c317edecd10
joshuabenedict-665 <joshuabenedict665@gmail.com>
2025-07-31 15:21:18 +0530

    changes in predictor.py

--- backend/ml/predictor.py
+++ backend/ml/predictor.py
@@ -1,3 +1,12 @@
+import numpy as np
+from keras.models import load_model
+import pickle
+import os
+model_path=os.path.join("backend", "ml", "model", "food_safety_model.h5");
+scaler_path=os.path.join("backend", "ml", "model", "scaler.pkl");
+model=load_model(model_path)
+with open(scaler_path, 'rb') as file:
+    scaler = pickle.load(file)
 def match_surplus_to_needy(food_type, quantity, location):
     return {
         "suggested_organization": "Local Shelter A",
@@ -6,6 +15,7 @@ def match_surplus_to_needy(food_type, quantity, location):
     }
 def classify_food(data: dict) -> str:
     # Dummy logic (can be replaced with actual model)
-    if data["hours_since_prepared"] < 4 and data["temperature"] < 35:
-        return "Safe"
-    return "Unsafe"
+    features = np.array([[data["quantity"], data["hours_since_prepared"], data["temperature"]]])
+    features_scaled = scaler.transform(features)
+    prediction = model.predict(features_scaled)[0][0]
+    return "Safe" if prediction > 0.5 else "Unsafe"
